2b4b2e68c13b98def14e98f95dda602d6235d5f7
Nicolas <nicolas.roussel.pro@gmail.com>
2022-02-01 12:03:37 +0100

    liste lexiques

--- src/Controller/LexiconController.php
+++ src/Controller/LexiconController.php
@@ -2,12 +2,18 @@
 
 namespace App\Controller;
 
-use App\Repository\LexiconRepository;
+use App\Entity\Lexicon;
+use App\Form\LexiconType;
+use Gitonomy\Git\Repository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use App\Repository\LexiconRepository;
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 /**
  * @Route("/lexicon", name="lexicon_")
@@ -18,8 +24,35 @@ class LexiconController extends AbstractController
      * @Route("", name="index")
      * @IsGranted("ROLE_USER")
      */
-    public function index(): Response
+    public function index(LexiconRepository $repository): Response
     {
-        return $this->render('lexicon/index.html.twig');
+        $lexicons = $repository->findAll();
+
+        return $this->render('lexicon/index.html.twig', ['lexicons' => $lexicons]);
+    }
+
+    /**
+     * @Route("/add", name="new")
+     */
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $lexicon = new Lexicon();
+        $form = $this->createForm(LexiconType::class, $lexicon);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var \App\Entity\User $user */
+            $user = $this->getUser();
+            $lexicon->setUser($user);
+            $entityManager->persist($lexicon);
+            $entityManager->flush();
+            $this->addFlash('success', 'Lexicon ajouté avec succès');
+
+            return $this->redirectToRoute('lexicon_index');
+        }
+
+        return $this->render('lexicon/new.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
--- src/Controller/WordController.php
+++ src/Controller/WordController.php
@@ -8,11 +8,11 @@ use Gitonomy\Git\Repository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use App\Repository\WordRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/word", name="word_")
