0ae4399eb89233f28f00bbef305c3cc94ddb243b
enfel <enfel.mhamedi@yahoo.fr>
2022-02-02 16:19:28 +0100

    style commit

---
+++ src/Controller/AboutController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/", name="about_")
+ */
+class AboutController extends AbstractController
+{
+    /**
+     * @Route("", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('about/index.html.twig');
+    }
+}
+{
+
+}
