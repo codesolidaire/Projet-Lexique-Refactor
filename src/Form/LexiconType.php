3b20ba19817cf829deb7fa6e9ce2b1c42cfc9487
enfel <enfel.mhamedi@yahoo.fr>
2022-02-03 14:28:42 +0100

    style add lexicon

--- src/Form/LexiconType.php
+++ src/Form/LexiconType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
-
 class LexiconType extends \Symfony\Component\Form\AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
